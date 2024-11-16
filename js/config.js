0e5db4e338ac6309dfbe569d6cb0efa6bbf209df
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-11-16 09:41:36 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_F6Ssvpvsk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6f34qsap89v81tma6ksjmv3slg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
