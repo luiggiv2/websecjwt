bb067658e0847f5acd6243d35433f1998e700941
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-11-16 09:45:12 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_F6Ssvpvsk', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6f34qsap89v81tma6ksjmv3slg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
